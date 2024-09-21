878902813b6afd6d1194a548ede2e0b50e6a22ef
Marcos Porto <mosther_marcos007@hotmail.com>
2024-09-21 11:58:33 -0300

    first commit

---
+++ src/http/get-pending-goals.ts
@@ -0,0 +1,14 @@
+type PendingResponse = {
+        id: string;
+        title: string;
+        desiredWeeklyFrequency: number;
+        completionCount: number;
+    }[]
+  
+
+export async function getPendingGoals(): Promise<PendingResponse>{
+    const response = await fetch('http://localhost:3333/pending-goals')
+    const data = await response.json()
+
+    return data.pendingGoals
+}
